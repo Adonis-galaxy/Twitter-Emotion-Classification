d288fa321ab33dc83da7a0dc5815e30e92b82373
xuyt1 <xuyt1@shanghaitech.edu.cn>
2021-12-22 12:29:56 +0800

    small fix

--- src/base.py
+++ src/base.py
@@ -11,6 +11,8 @@ from label_preprocess import label_preprocessing
 from train import trainFunc
 from val import validation
 from fileloader import load_text,load_label
+from build_histogram import histogram_building
+
 def baseline(model = MLPRegressor( hidden_layer_sizes=(16),  activation='tanh', solver='adam')):
     # %%
     # load datasets
@@ -21,15 +23,12 @@ def baseline(model = MLPRegressor( hidden_layer_sizes=(16),  activation='tanh', 
     test_text = load_text("test_text")
     test_labels = np.array(load_label("test_labels"))
     # %%
-    from build_histogram import histogram_building
     histogram = histogram_building(train_text)
     num_feature = len(histogram) # 12887
     tokens = list(histogram.index)
     # %%
     train_data,num_train = text_preprocessing(train_text,tokens,num_feature)
-    train_labels_processed = label_preprocessing(num_train,train_labels)
     val_data,num_val = text_preprocessing(val_text,tokens,num_feature)
-    val_labels_processed = label_preprocessing(num_val,val_labels)
     # %%
-    trainFunc(model,train_data,train_labels_processed,train_labels,num_train)
+    trainFunc(model,train_data,train_labels,num_train)
     validation(model,val_data,val_labels,num_val)
--- src/train.py
+++ src/train.py
@@ -4,10 +4,12 @@ from sklearn import svm
 import sklearn
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
-def trainFunc(model,train_data,train_labels_processed,train_labels,num_train):
+from label_preprocess import label_preprocessing
+def trainFunc(model,train_data,train_labels,num_train):
     # model=MLPRegressor( hidden_layer_sizes=(16),  activation='tanh', solver='adam')
     print("type of the model:",type(model))
     if type(model) == MLPRegressor:
+        train_labels_processed = label_preprocessing(num_train,train_labels)
         model.fit(train_data.T, train_labels_processed)
         train_accuracy = sum(model.predict(train_data.T).argmax(1) == train_labels) / num_train
         print("train acc",train_accuracy)
