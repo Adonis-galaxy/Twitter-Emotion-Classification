503a639cd197d2fd7251aa373725c58d53e1675d
Rider2019 <3249273103@qq.com>
2022-01-08 15:14:53 +0800

    mlp modify

--- src/naive_bayes.py
+++ src/naive_bayes.py
@@ -5,11 +5,6 @@ from sklearn.naive_bayes import BernoulliNB
 from sklearn.naive_bayes import ComplementNB  
 X, y = load_iris(return_X_y=True)
 from base import baseline
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=0)
-
 gnb = ComplementNB(alpha=1.1)
-# y_pred = gnb.fit(X_train, y_train).predict(X_test)
-# print("Number of mislabeled points out of a total %d points : %d"% (X_test.shape[0], (y_test != y_pred).sum()))
 baseline(gnb)
-# print(type(gnb))
 
