ba68b9ecc6dcfa81218d68b7febf2559b026ae11
Rider2019 <3249273103@qq.com>
2022-01-08 15:50:16 +0800

    test

--- src/naive_bayes.py
+++ src/naive_bayes.py
@@ -3,7 +3,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB  
 from sklearn.naive_bayes import BernoulliNB  
 from sklearn.naive_bayes import ComplementNB  
-X, y = load_iris(return_X_y=True)
 from base import baseline
 gnb = ComplementNB(alpha=1.1)
 baseline(gnb)
